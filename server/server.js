cbad5c4f1a329811268d5e90444854935f536976
Christophe Johanny <c.johanny@gmail.com>
2018-06-12 13:16:06 +0200

    add port for heroku

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ const path = require('path')
 const express = require('express')
 
 const publicPath = path.join(__dirname, '../public')
-
+const port = process.env.PORT || 3000
 let app = express()
-const port = 3000
+
 
 app.use(express.static(publicPath))
 
