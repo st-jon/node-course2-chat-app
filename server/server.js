73d8aa26b5adebf4217f352e62b7d3e89eade83a
Christophe Johanny <c.johanny@gmail.com>
2018-06-12 15:16:55 +0200

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New user connected')
 
+    socket.emit('newMessage', {
+        from:'Admin',
+        text: 'welcome to the Chat App !',
+        createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'A new user just joined',
+        createdAt: new Date().getTime()
+    } )
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message)
         io.emit('newMessage', {
@@ -22,6 +34,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         })
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     })
 
     socket.on('disconnect', () => {
