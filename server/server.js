311a4ffebb921862e3e26b1b3b7840cebdaefdfb
Christophe Johanny <c.johanny@gmail.com>
2018-06-13 10:20:41 +0200

    scroll on bottom

--- server/server.js
+++ server/server.js
@@ -21,13 +21,12 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('newMessage', generateMessage('admin', 'a new user joined'))
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message)
         io.emit('newMessage', generateMessage(message.from, message.text))
         callback()
     })
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+        io.emit('newLocationMessage', generateLocationMessage('admin', coords.latitude, coords.longitude))
     })
 
     socket.on('disconnect', () => {
